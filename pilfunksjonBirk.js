e47c4deed22e65f7a85d2ca1904576e43cc7fabb
birk <besw05@gmail.com>
2022-04-06 10:11:38 +0200

    pilfunksjoner

---
+++ pilfunksjonBirk.js
@@ -0,0 +1,28 @@
+//Oppgave1
+//Lag en pilfunksjon uten parametere. Den skal skrive ut “Hello World” til konsollet. Gjør etfunksjonskall som beviser at den virker.
+const minFunksjon = _ => {
+    console.log("Hello world");
+}
+    
+
+//Oppgave2
+//Lag en pilfunksjon med en parameter. Funksjonen skal skrive ut parameteren til konsollet. Gjøret funksjonskall som beviser at den virker.Kan du gjøre noe spesielt når funksjonen kun har en parameter?
+const minFunksjon = (a) => {
+    console.log("output: ", a);
+    }
+    
+
+//Oppgave3
+//Lag en pilfunksjon med tre parametere. Funksjonen skal skrive ut alle parameterne til konsollet.Gjør et funksjonskall som beviser at den virker.
+const minFunksjon = (a, b, c) => {
+    console.log("output:" , a, b, c);
+}
+    
+
+//Oppgave4
+//Lag en pilfunksjon som bruker rest operatøren. Skriv ut hele variabelen med ett kall.-Hvis du vil demonstrere at du er avansert kan du skrive ut innholdet i variabelen med en løkkeav eget valg.
+//...........
+
+//Oppgave5
+//Vet du om noen praktiske forskjeller på pilfunksjoner og funksjoner som er definert mednøkkelordet “function”. Skriv hva du vet.
+//vet ingen ting
